53c73b035d6f75c0875d7395f1dd2b78c96f8231
Tetiana Karpa <44815425+KT9045@users.noreply.github.com>
2019-02-04 16:02:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var number = prompt("What's your number?");
+console.log (number);
+function fib(n) {
+    var a = 1,
+        b = 1;
+    for (var i = 3; i <= n; i++) {
+        var c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+alert( fib(number) );
